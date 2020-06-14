3c526f274a90807b873d4a833938620e7b2800eb
KRVPerera <2173530+KRVPerera@users.noreply.github.com>
2020-06-14 13:49:40 +0530

    Add new crons

---
+++ scripts/Ballerina/git/syncSrc2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+osascript -e 'display notification "Source2 build started"' with title "syncSrc2.sh cronjob"
+osascript -e 'say "Source2 build started"'
+
+cd /Users/rukshanp/Projects/Ballerina/ballerina-lang
+git checkout master
+git pull origin master
+git push
+./gradlew build -x createJavadoc -x check -x test -x generateDocs -x npmBuild
+
+osascript -e 'display notification "Source2 build finished"' with title "syncSrc2.sh cronjob"
+osascript -e 'say "Source2 build finished"'
