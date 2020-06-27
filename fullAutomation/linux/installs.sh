e007e077bf9bc52c4cb887328b54bbf28ab7408c
KRVPerera <2173530+KRVPerera@users.noreply.github.com>
2020-06-28 02:11:11 +0530

    add zplug

--- fullAutomation/linux/installs.sh
+++ fullAutomation/linux/installs.sh
@@ -31,6 +31,7 @@ zshInstall=`grep /zsh$ /etc/shells | tail -1`
 if [[ -z $zshInstall ]]; then
     sudo apt install zsh
     sudo apt install curl
+    curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
