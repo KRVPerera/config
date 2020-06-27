8dd67a0a579615ea6513e7750444fa8da51df094
KRVPerera <rukshan.viduranga@gmail.com>
2020-06-27 21:38:15 +0530

    linux script git cache

--- fullAutomation/linux/installs.sh
+++ fullAutomation/linux/installs.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
 sudo apt install vim
+sudo apt install git
+
+## git setup
+git config --global user.name "KRVPerera"
+git config --global user.email "rukshan.viduranga@gmail.com"
+git config --global color.ui auto
+git config --global core.editor "vim --wait"
+git config --global merge.conflictstyle diff3
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=3600'
+
 curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh
