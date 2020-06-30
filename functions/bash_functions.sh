161ae1bd20861f54b3a82f689b6d138a1afa475b
KRVPerera <rukshan.viduranga@gmail.com>
2020-06-30 18:50:37 +0530

    Fix runBBE and extractBal extention processing

--- functions/bash_functions.sh
+++ functions/bash_functions.sh
@@ -4,49 +4,56 @@ echo "Loading functions : $0"
 
 find_ballerina_file () {
     FILE=$1
-    FILE_EXT={$1:-4}
     debug_on=false
 
     if [[ $2 == "debug" ]]; then
         debug_on=true
     fi
 
-
     if [ -f "$FILE" ]; then
         echo $FILE
+        return 0
     fi
 
     if [[ $debug_on == true ]]; then
         echo "$FILE not found assuming a ballering file"
     fi
 
-    if [[ ${FILE:-1} == "." ]]; then
-        if [[ $debug_on == true ]]; then
-            echo "Extention is not given. Assuming '.bal'"
-        fi
+    LAST_CHAR="${FILE: -1}"
+    if [[ $LAST_CHAR == "." ]]; then
         FILE=${FILE}bal
+        if [[ $debug_on == true ]]; then
+            echo "Extention is not given. Assuming '.bal' : $FILE"
+        fi
     fi
 
     # TODO: fix this issue :(
     if [ "${#FILE}" -le 4 ]; then
-        if [[ $debug_on == true ]]; then
-            echo "File does not have a '.bal' extention"
-        fi
         FILE=${FILE}.bal
+        if [[ $debug_on == true ]]; then
+            echo "File is shorter to have an extention : $FILE"
+        fi
     fi
 
+    FILE_EXT="${FILE: -4}"
     if [[ ! $FILE_EXT == ".bal" ]]; then
-        if [[ $debug_on == true ]]; then
-            echo "File does not have a '.bal' extention"
-        fi
         FILE=${FILE}.bal
+        if [[ $debug_on == true ]]; then
+            echo "File is not a ballerina file : $FILE"
+        fi
     fi
 
     if [[ $debug_on == true ]]; then
-        echo "Found : $FILE"
+        echo "Final name : $FILE"
     fi
 
     if [[ -f "$FILE" ]]; then
         echo $FILE
+        return 0 
+    else
+        if [[ $debug_on == true ]]; then
+            echo "Final name : $FILE"
+        fi
+        return 1
     fi
 }
