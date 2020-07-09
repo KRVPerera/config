1cf1f27438da7a632e068adf438021b11e05a667
KRVPerera <rukshan.viduranga@gmail.com>
2020-07-09 09:38:26 +0530

    new scripts and ballerina build script changes

--- scripts/Ballerina/balBuildNoBalos.sh
+++ scripts/Ballerina/balBuildNoBalos.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 ./gradlew build -x :jballerina-integration-test:test -x :plugin-vscode:test -x createBalo -x createJavadoc -x check -x test -x generateDocs -x npmBuild -x createBir -x :ballerina-rt:jar
-osascript -e 'say "Build finished"'
+buildStatus=$?
+if [[ $buildStatus -ne 0 ]]; then
+    osascript -e 'display notification "build FAILED" with title "BUILD FAILED" subtitle "Build ballerina"'
+    osascript -e 'say "Ballerina build FAILED"'
+    exit 1
+fi
+osascript -e 'say "Ballerina Build SUCCESSFUL"'
+sleep 1
