25b31ff55eff9ecfb5dc158718c81210deed631e
KRVPerera <2173530+KRVPerera@users.noreply.github.com>
2020-08-12 16:40:38 +0530

    update 12th August

--- scripts/Ballerina/balBuildOnly.sh
+++ scripts/Ballerina/balBuildOnly.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 osascript -e 'display notification "Ballerina build started" with title "Build started" subtitle "Build Ballerina"'
+echo "./gradlew build -x createJavadoc -x check -x test -x generateDocs -x npmBuild $@"
 ./gradlew build -x createJavadoc -x check -x test -x generateDocs -x npmBuild $@
 buildStatus=$?
 if [[ $buildStatus -ne 0 ]]; then
     osascript -e 'display notification "build FAILED" with title "BUILD FAILED" subtitle "Build ballerina"'
     osascript -e 'say "Ballerina build FAILED"'
+    git restore misc/testerina/modules/testerina-core/src/main/ballerina/Ballerina.toml
     exit 1
 fi
-osascript -e 'say "Ballerina Build SUCCESSFUL"'
+osascript -e 'say "Congratulations Ballerina Build without test SUCCESSFUL"'
+git restore misc/testerina/modules/testerina-core/src/main/ballerina/Ballerina.toml
 sleep 1
