4f3c59edf2b4c6a738e1e7f27e59c41c6e68c81b
KRVPerera <rukshan.viduranga@gmail.com>
2022-02-16 21:43:06 +0530

    add terminator config

--- fullAutomation/linux/installs.sh
+++ fullAutomation/linux/installs.sh
@@ -11,8 +11,8 @@ else
 fi
 
 ## git setup
-git config --global user.name ""
-git config --global user.email ""
+git config --global user.name "KRVPerera"
+git config --global user.email "rukshan.viduranga@gmail.com"
 git config --global color.ui auto
 git config --global core.editor "vim --wait"
 git config --global merge.conflictstyle diff3
@@ -20,27 +20,20 @@ git config --global credential.helper cache
 git config --global credential.helper 'cache --timeout=3600'
 
 ## utils
+### monitoring tools
 sudo apt install htop
 sudo apt install pydf
+
 sudo apt install vbetool
 ## sudo vbetool dpms off
-sudo apt-get install git-extras
+
+## terminal
+sudo apt install terminator
+
+## dev tools
+sudo apt install git-extras
 
 # Oh my zsh
-zshInstall=`grep /zsh$ /etc/shells | tail -1`
-if [[ -z $zshInstall ]]; then
-    sudo apt install zsh
-    sudo apt install curl
-    curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
-else
-    echo ""
-    echo "##############################################"
-    echo "####    zsh is already installed          ####"
-    echo "##############################################"
-fi
 
 ## Java installation
 if [[ ! -f /usr/bin/java ]] ; then
@@ -67,10 +60,6 @@ fi
 cd $configPath
 git pull origin master
 
-echo "Copying zshrc"
-mv ~/.zshrc ~/.zshrc.bak
-ln -s $configPath/zshrc.linux ~/.zshrc
-
 echo "Copying aliases"
 mv ~/bash_aliases.sh ~/bash_aliases.bak
 ln -s $configPath/bash_aliases.sh ~/bash_aliases.sh
@@ -78,6 +67,10 @@ ln -s $configPath/bash_aliases.sh ~/bash_aliases.sh
 echo "Copying bashrc"
 mv ~/.bashrc ~/.bashrc.bak
 ln -s $configPath/bashrc ~/.bashrc
+
+echo "Copying terminator config"
+mv ~/.config/terminator/config ~/.config/terminator/config
+ln -s $configPath/terminator/config ~/.config/terminator/config
 
 ## vim setup
 if [[ ! -f /usr/bin/vim ]] ; then
@@ -98,3 +91,5 @@ else
     vim --version
     echo "##############################################"
 fi
+
+#source install_zsh.sh
