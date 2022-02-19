55a0eb5b658640ca7756d1ad6a11dec08a4647f3
KRVPerera <rukshan.viduranga@gmail.com>
2022-02-19 14:49:23 +0530

    move ripgrep to rust installer

--- fullAutomation/linux/install_rustc_things.sh
+++ fullAutomation/linux/install_rustc_things.sh
@@ -5,7 +5,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
 
 ## Install webassembly support with rust
-sudo apt install libssl-dev
+sudo apt install -y libssl-dev
 cargo install cargo-generate
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 
@@ -13,7 +13,8 @@ curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 cargo install du-dust
 cargo install exa
 cargo install bat
-sudo apt install fd-find
 cargo install tokei
 cargo install procs
 cargo install alacritty
+sudo apt install -y ripgrep
+sudo apt install -y fd-find
