f3880f22c9b48867ed55521ef18b10d48845e524
KRVPerera <rukshan.viduranga@gmail.com>
2022-02-19 19:24:09 +0530

    change mode key

---
+++ fullAutomation/linux/xmonad.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+## some are duplicated here but it is okay
+sudo apt install -y xmonad libghc-xmonad-dev libghc-xmonad-contrib-dev 
+libghc-xmonad-contrib-doc 
+sudo apt install -y dmenu xterm ranger thunar
