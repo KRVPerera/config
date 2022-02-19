2829de9c132c96cc5d72fa32d9525407ebffc896
KRVPerera <2173530+KRVPerera@users.noreply.github.com>
2022-02-19 22:04:30 +0530

    add xmobar

--- fullAutomation/linux/xmonad.sh
+++ fullAutomation/linux/xmonad.sh
@@ -2,4 +2,5 @@
 ## some are duplicated here but it is okay
 sudo apt install -y xmonad libghc-xmonad-dev libghc-xmonad-contrib-dev 
 libghc-xmonad-contrib-doc 
-sudo apt install -y dmenu xterm ranger thunar
+sudo apt install -y dmenu xterm ranger thunar nitrogen compton
+sudo apt install -y xmobar
